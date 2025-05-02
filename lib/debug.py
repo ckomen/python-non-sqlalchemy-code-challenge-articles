8efd771f1364d64c0182ab3df3184a7edf6c6ca1
ckomen <cjepkorir09@gmail.com>
2025-05-02 22:03:15 +0300

    complete

--- lib/debug.py
+++ lib/debug.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
-import ipdb;
 
-from classes.many_to_many import Article
-from classes.many_to_many import Author
-from classes.many_to_many import Magazine
+import ipdb
+
+from classes.many_to_many import Article, Author, Magazine
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+    print("HELLO! :) Let's debug :vibing_potato:")
 
+    # Sample data
+    author1 = Author("Carrie Bradshaw")
+    author2 = Author("Topher Grace")
 
-    # don't remove this line, it's for debugging!
+    mag1 = Magazine("Vogue", "Fashion")
+    mag2 = Magazine("Architectural Digest", "Architecture")
+
+    article1 = Article(author1, mag1, "How to wear a tutu with style")
+    article2 = Article(author1, mag2, "Buildings and You")
+    article3 = Article(author1, mag1, "Fashion for Fall")
+    article4 = Article(author1, mag1, "The Skirt Awakens")
+    article5 = Article(author2, mag1, "Actor Turned Author")
+
+    # Test calls
+    print("Author1 Articles:", author1.articles())
+    print("Author1 Magazines:", author1.magazines())
+    print("Magazine1 Contributors:", mag1.contributors())
+    print("Magazine1 Article Titles:", mag1.article_titles())
+    print("Magazine1 Contributing Authors:", mag1.contributing_authors())
+    print("Top Publisher:", Magazine.top_publisher())
+
     ipdb.set_trace()
